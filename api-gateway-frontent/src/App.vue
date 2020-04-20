cee56e490501f70819efeee86c42a9f3a8f4507c
Jason Nguyen <jnguyen7539@gmail.com>
2020-04-20 11:55:20 -0700

    added views and link bar to frontend

--- api-gateway-frontent/src/App.vue
+++ api-gateway-frontent/src/App.vue
@@ -1,32 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app="true" color="deep-purple accent-4" dark="true">
+      <v-toolbar-title>Web API Gateway</v-toolbar-title>
+    </v-app-bar>
+
+
+    <v-navigation-drawer app="true" expand-on-hover="true">
+      <v-list-item v-for="item in list" :key="item.title" :to="item.link">
+        <v-list-item-content>
+          <v-list-tile :to="item.link">{{ item.title }}</v-list-tile>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  components: {},
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      list: [
+        { title: "Home", link: "/" },
+        { title: "Login", link: "Login" },
+        { title: "Register Team", link: "RegisterTeam" },
+        { title: "Register Services", link: "RegisterService" },
+        { title: "Manage Services", link: "ManageService" },
+        { title: "Manage Team", link: "ManageTeam" },
+        { title: "Service Discovery", link: "ServiceDiscovery" },
+        { title: "About", link: "About" },
+      ],
+    };
+  },
+};
+</script>
