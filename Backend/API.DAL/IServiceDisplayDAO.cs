aaa81c8201de53e77abdd25c898c1a0dd6c97b80
albertdu233 <32620395+albertdu233@users.noreply.github.com>
2020-04-26 11:31:02 -0700

    Modify the servicedisplay with new context

---
+++ Backend/API.DAL/IServiceDisplayDAO.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+using API.Models.gateway;
+
+namespace DataAccessLayer
+{
+    public interface IServiceDisplayDAO
+    {
+        ICollection<Service> GetAllData(string input);
+        bool IfTeamExist(string apiKey);
+    }
+}
