d6b972207824fb6170ab67763a6eb99b6e3a0686
Jason Nguyen <jnguyen7539@gmail.com>
2020-04-28 17:30:11 -0700

    updated docs and unit test

--- api-gateway-frontent/tests/e2e/specs/ServiceManagementTest.js
+++ api-gateway-frontent/tests/e2e/specs/ServiceManagementTest.js
@@ -1,26 +1,5 @@
 // PASS Update service privacy 
-describe("Pass update service privacy", () => {
-    it("Input text and submit", () => {
-     // Arrange go to login view type in credentials
-     cy.visit("http://localhost:8080/#/login");
 
-     cy.get("input[id='Username']").type("ASDASDASD");
-     cy.get("input[id='Password']").type("ThisisavalidPassword");
- 
-     cy.get("button[id='Login']").click();
- 
-     cy.url().should("eq", "http://localhost:8080/#/");
-
-     cy.visit("http://localhost:8080/#/ManageService");
-
-    
-
-     cy.get("button").select(1);
-     
-     cy.contains("Service successfully created");
-     cy.contains("Accept").click();
-    });
-  });
 
 // PASS Delete service
 
